3165c9281762cd08ee5ca1a9365ce21b6c6f2a80
Rizky Gunardi <rgunardi97@gmail.com>
2023-04-06 20:38:15 +0700

    Testing TouchMe

---
+++ Scripts/Signup Reapet Password FAIL/Script1680706122297.groovy
@@ -0,0 +1,49 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('/Users/rizky/Katalon Studio/Touch Me/Apk/Touchme.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/Notif/View-signup'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Field/Full Name'), 0)
+
+Mobile.setText(findTestObject('Field/Full Name'), 'kynaran', 0)
+
+Mobile.tap(findTestObject('Object Repository/Field/Email'), 0)
+
+Mobile.setText(findTestObject('Field/Email'), 'daftarnyadisini@gmail.com', 0)
+
+Mobile.tap(findTestObject('Object Repository/Field/Password'), 0)
+
+Mobile.setText(findTestObject('Field/Password'), 'AkuCintaKamuSelamanya', 0)
+
+Mobile.tap(findTestObject('Object Repository/Field/Repeat Password'), 0)
+
+Mobile.setText(findTestObject('Field/Repeat Password'), 'Aku', 0)
+
+Mobile.hideKeyboard()
+
+Mobile.tap(findTestObject('Object Repository/Notif/Button'), 0)
+
+Mobile.verifyElementText(findTestObject('Notif/View Reapet Password Error'), '')
+
+Mobile.delay(3, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.closeApplication()
+
---
+++ Test Suites/Touchme.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Melakukan Testing pada fungsi login dan signup dengan testing positive dan negative.</description>
+   <name>Touchme</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>106ebd79-acf6-49af-8d72-38fd931ecc7b</testSuiteGuid>
+   <testCaseLink>
+      <guid>b441590d-71a6-4e40-bc67-fa4816558782</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Signup PASS</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3c43e024-3d63-48e5-9c00-795a62f871c5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Signup Email FAIL</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d9ab40b8-99f9-494a-9098-472a69d09460</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Signup Password FAIL</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6e03f85-0696-4e2d-8d75-d42bf8090711</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Signup Reapet Password FAIL</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7e612686-cf45-4119-b49c-2a6d80b30b52</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login PASS</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6e5f162-3207-49fb-97d9-456059ca1dba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Email Fail</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3a7b6db3-3961-4557-b472-f8982f3559f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Password Fail</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
